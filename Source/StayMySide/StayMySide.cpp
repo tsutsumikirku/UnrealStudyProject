1f1187c28a85dd65c115308685e8643358052670
tsutsumi <tsutsumikiriku@gmail.com>
2025-01-09 03:19:14 +0900

    [Add]初回プッシュ

---
+++ Source/StayMySide/StayMySide.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StayMySide.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StayMySide, "StayMySide" );
